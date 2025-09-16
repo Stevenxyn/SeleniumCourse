faf41695212bf2a9771c924688f41834d90f937a
MercyDev <cslopezmontano@gmail.com>
2025-09-16 00:15:15 -0500

    Aprendi como iniciar el driver desde diferentes navegadores y configurarlos, hice la prueba en Chrome, firefox y Edge todos funcionando correctamente

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# SeleniumCourse
+## Configurar SELENIUM 
 
-Hola que tal
+
+Descargamos Selenium Server Grid [Selenium Descargas](https://www.selenium.dev/downloads/).
+
+Al descargar iremos al **IDE** en el proyecto donde tenemos creado damos click izquiero en **Build Path > Configure Build Path** y en **Libreries > Classpath** damos en **Add External JARS** y agregamos lo descargado, se agregara en el proyecto SeleniumServer.
--- src/DemoAutomation.java
+++ src/DemoAutomation.java
@@ -1,9 +1,30 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DemoAutomation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Con esto le indicamos donde podra encontrar el driver de chromeDriver, lo tenemos que tener descargado previamente
+		System.setProperty("webdriver.chrome.driver", "C:\\BrowserDrivers\\drivers\\chromedriver_V140.0.7339.82.exe");
+		
+		//System.setProperty("webdriver.firefox.driver", "C:\\BrowserDrivers\\drivers\\geckodriver_142.0.1.exe");
+		//System.setProperty("webdriver.edge.driver", "C:\\BrowserDrivers\\drivers\\msedgedriver_V140.0.3485.66.exe");
 
+
+		//Creamos el objeto chromedriver, driver nos sirve para manejar el navegador en este caso sera Chrome
+		ChromeDriver driver = new ChromeDriver();
+
+		//FirefoxDriver driver2 = new FirefoxDriver();
+		//EdgeDriver driver3 = new EdgeDriver();
+		
+		
+		
+		driver.get("https://www.google.com/");
+		
+		
+		
 	}
 
 }
